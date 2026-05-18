1a0f4fc663dd887d474edc1891d9c21e1bb698a0
Jamie B <jamie1612@gmail.com>
2026-05-18 20:44:09 +0100

    Emit raw CELT benchmark timings as RTFx

--- tools/run_raw_celt_bench.sh
+++ tools/run_raw_celt_bench.sh
@@ -67,7 +67,9 @@ echo "Repeats: $REPEATS, seconds: $AUDIO_SECONDS, mode: $MODE" >&2
 rust_out="$("$rust_bin" --repeats "$REPEATS" --seconds "$AUDIO_SECONDS" --mode "$MODE")"
 c_out="$("$c_bin" --repeats "$REPEATS" --seconds "$AUDIO_SECONDS" --mode "$MODE")"
 
-printf '%s\n%s\n' "$rust_out" "$c_out" | awk -F '\t' '
+rt_factor=$((AUDIO_SECONDS * 1000))
+
+printf '%s\n%s\n' "$rust_out" "$c_out" | awk -F '\t' -v rt_factor="$rt_factor" '
   $1 == "impl" { next }
   {
     key = $2 "|" $3 "|" $5
@@ -85,16 +87,16 @@ printf '%s\n%s\n' "$rust_out" "$c_out" | awk -F '\t' '
     max_packet[$1, key] = $10
   }
   END {
-    print "| Mode | Frame | Bitrate | Rust enc | Enc vs C | Rust dec | Dec vs C | C enc | C dec | Rust bytes | C bytes | Rust pkt | C pkt |"
+    print "| Mode | Frame | Bitrate | Rust enc (RTFx) | Enc vs C | Rust dec (RTFx) | Dec vs C | C enc (RTFx) | C dec (RTFx) | Rust bytes | C bytes | Rust pkt | C pkt |"
     print "| --- | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: |"
     for (i = 1; i <= count; i++) {
       key = order[i]
       if ((("rust" SUBSEP key) in encode) && (("c" SUBSEP key) in encode)) {
         enc_delta = 100.0 * (encode["rust", key] - encode["c", key]) / encode["c", key]
         dec_delta = 100.0 * (decode["rust", key] - decode["c", key]) / decode["c", key]
-        printf "| %s | %.1f ms | %d kb/s | %.2f ms | %+.1f%% | %.2f ms | %+.1f%% | %.2f ms | %.2f ms | %d | %d | %d-%d | %d-%d |\n", \
-          mode[key], frame_ms[key], bitrate[key] / 1000, encode["rust", key], enc_delta, \
-          decode["rust", key], dec_delta, encode["c", key], decode["c", key], \
+        printf "| %s | %.1f ms | %d kb/s | %.4fx | %+.1f%% | %.4fx | %+.1f%% | %.4fx | %.4fx | %d | %d | %d-%d | %d-%d |\n", \
+          mode[key], frame_ms[key], bitrate[key] / 1000, encode["rust", key] / rt_factor, enc_delta, \
+          decode["rust", key] / rt_factor, dec_delta, encode["c", key] / rt_factor, decode["c", key] / rt_factor, \
           bytes["rust", key], bytes["c", key], \
           min_packet["rust", key], max_packet["rust", key], min_packet["c", key], max_packet["c", key]
       }
